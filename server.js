b84b1cdea250a195be95ff6ada6eb90a8e96b8a5
Rashmi <poddar.rashmi@gmail.com>
2019-11-05 17:41:08 -0500

    Added endpoints for creating and reading cars

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const carsRoute = require('./routes/carRoutes');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/cars', carsRoute);
 
 server.get('/', (req, res) => {
   res.status(200).send({message: 'Go to /cars'});
